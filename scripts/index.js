958d3855967a09db9c667ad88f9ac16d5f0aa964
Mary Holst <maryzqholst@gmail.com>
2026-04-13 10:19:20 -0400

    New update for ny project

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,22 @@
-editProfile = document.querySelector(".profile__edit-btn")
+const editProfile = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editModalClose = editProfileModal.querySelector(".modal__close");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+const editProfileNameInput = editProfileModal.querySelector("#name-input");
+const editProfileDescriptionInput = editProfileModal.querySelector("#description-input");
 
-editProfileModal = document.querySelector("#edit-profile-modal")
 
-editModalClose = editProfileModal.querySelector(".modal__close")
+const newPost = document.querySelector(".profile__add-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const newModalClose = newPostModal.querySelector(".modal__close");
 
-newPost = document.querySelector(".profile__add-btn")
 
-newPostModal = document.querySelector("#new-post-modal")
-
-newModalClose = newPostModal.querySelector(".modal__close")
 
 editProfile.addEventListener("click", function() {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -25,3 +31,11 @@ newPost.addEventListener("click", function() {
 newModalClose.addEventListener("click", function() {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
